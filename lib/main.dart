fd67b49710e3e2b1281c20744f45bea75082af94
sergeilobachev <gsvlobachev@gmail.com>
2022-11-14 14:01:21 +0300

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:megaohm_app/app_settings/app_view.dart';
 import 'app_settings/dependency_injection.dart';
@@ -6,9 +7,12 @@ import 'app_settings/parts/hive_init.dart';
 
 
 Future<void> main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await Hive.initFlutter();
   await Hive.openBox('RegistrationBox');
   hiveInit();
-  runApp(MyApp());
   dependencyInjection();
+  runApp(MyApp());
+  FlutterNativeSplash.remove();
 }
