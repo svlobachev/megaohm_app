477887940c79696a8dafe2a272f809e93edf593d
sergeilobachev <gsvlobachev@gmail.com>
2022-11-29 12:01:12 +0300

    added logout
    Took 32 seconds

--- lib/widgets/navigation/nav_drawer/navDrawer_logout_dialog_model.dart
+++ lib/widgets/navigation/nav_drawer/navDrawer_logout_dialog_model.dart
@@ -2,10 +2,6 @@ import 'package:hive/hive.dart';
 
 class NavDrawerShowDialogModel {
   final Box _box = Hive.box('RegistrationBox');
-  // final ConfirmationAPIService _confirmationAPIService = Get.find();
-  // final ConfirmationPageController _confirmationPageController = Get.find();
-  // final RegistrationPageController _registrationPageController = Get.find();
-  // final RegistrationAPIService _registrationAPIService = Get.find();
 
   dropRegistrationData() {
     _box.put('registrationStatus', '');
@@ -13,13 +9,5 @@ class NavDrawerShowDialogModel {
     _box.put('tokenRt', '');
     _box.put('phone', '');
     _box.put('email', '');
-
-    // _confirmationAPIService.tokenRt = '';
-    // _confirmationAPIService.tokenAt = '';
-    // _registrationAPIService.token = '';
-    // _confirmationPageController.codeFieldIsFilled = '';
-    // _registrationPageController.emailFieldIsFilled = '';
-    // _registrationPageController.phoneFieldIsFilled = '';
-    // _registrationPageController.nameFieldIsFilled = '';
   }
 }
